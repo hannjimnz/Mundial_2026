e3411e32dce9af27f9c4ca4380bfebdf8096967b
hannjimnz <hannjimnz@gmail.com>
2026-06-30 08:16:32 -0600

    Final

--- lib/core/api_constants.dart
+++ lib/core/api_constants.dart
@@ -4,6 +4,10 @@ class ApiConstants {
   static const String baseUrl =
       "http://10.0.2.2:8000";
 
+
+
+
+
   // Si usas celular físico:
   // static const String baseUrl =
   // "http://192.168.X.X:8000";
